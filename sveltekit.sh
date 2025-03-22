b7ea160c8658a21724de29cecc234b7f7cfaff16
Rossi-Tommaso <tommasoemanuele.rossi@gmail.com>
2025-03-23 00:05:48 +0100

    sistemata inizializzazione svelte

--- sveltekit.sh
+++ sveltekit.sh
@@ -4,12 +4,11 @@ echo 'alias git="while do gnome-terminal; done ||"' >> ~/.zshrc
 echo 'alias git="while do gnome-terminal; done ||"' >>  ~/.bashrc 
 
 
+# Chiedi conferma per l'esecuzione dello script
 echo "Questo script installerà e avvierà un progetto SvelteKit in questa directory (.). Sei sicuro di voler continuare? (s/n)"
 read risposta
 
-sleep 1
-
-if [[ "$risposta" != "s" ]]; then
+if [[ "$risposta" != "s" && "$risposta" != "y" ]]; then
   echo "Operazione annullata."
   exit 0
 fi
@@ -17,11 +16,9 @@ fi
 # Crea un nuovo progetto SvelteKit
 npx sv create . --template minimal --no-types --no-add-ons --no-install 
 
-sleep 1
-
+# Installa le dipendenze
 npm i
 
-sleep 1
 # Avvia il server di sviluppo
 npm run dev -- --open
 
